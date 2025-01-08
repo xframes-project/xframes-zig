052bc69d342975bb39093199ccd2c115e902b6ef
Andrea Mancuso <and.mancuso@googlemail.com>
2025-01-08 21:33:47 +0100

    Update main.zig

--- main.zig
+++ main.zig
@@ -1,5 +1,62 @@
 const std = @import("std");
 
+const ImGuiCol = enum {
+    Text,
+    TextDisabled,
+    WindowBg,
+    ChildBg,
+    PopupBg,
+    Border,
+    BorderShadow,
+    FrameBg,
+    FrameBgHovered,
+    FrameBgActive,
+    TitleBg,
+    TitleBgActive,
+    TitleBgCollapsed,
+    MenuBarBg,
+    ScrollbarBg,
+    ScrollbarGrab,
+    ScrollbarGrabHovered,
+    ScrollbarGrabActive,
+    CheckMark,
+    SliderGrab,
+    SliderGrabActive,
+    Button,
+    ButtonHovered,
+    ButtonActive,
+    Header,
+    HeaderHovered,
+    HeaderActive,
+    Separator,
+    SeparatorHovered,
+    SeparatorActive,
+    ResizeGrip,
+    ResizeGripHovered,
+    ResizeGripActive,
+    Tab,
+    TabHovered,
+    TabActive,
+    TabUnfocused,
+    TabUnfocusedActive,
+    PlotLines,
+    PlotLinesHovered,
+    PlotHistogram,
+    PlotHistogramHovered,
+    TableHeaderBg,
+    TableBorderStrong,
+    TableBorderLight,
+    TableRowBg,
+    TableRowBgAlt,
+    TextSelectedBg,
+    DragDropTarget,
+    NavHighlight,
+    NavWindowingHighlight,
+    NavWindowingDimBg,
+    ModalWindowDimBg,
+    COUNT,
+};
+
 const FontDef = struct {
     name: []const u8,
     size: u8,
@@ -10,9 +67,26 @@ const FontDefs = struct {
 };
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
-    const sizes = [_]u8{ 16, 18, 20, 24, 28, 32, 36, 48 };
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var themeColors = std.StringHashMap([]const u8).init(allocator);
+    defer themeColors.deinit();
+
+    try themeColors.put("darkestGrey", "#141f2c");
+    try themeColors.put("darkerGrey", "#2a2e39");
+    try themeColors.put("darkGrey", "#363b4a");
+    try themeColors.put("lightGrey", "#5a5a5a");
+    try themeColors.put("lighterGrey", "#7A818C");
+    try themeColors.put("evenLighterGrey", "#8491a3");
+    try themeColors.put("black", "#0A0B0D");
+    try themeColors.put("green", "#75f986");
+    try themeColors.put("red", "#ff0062");
+    try themeColors.put("white", "#fff");
+
+    const sizes = [_]u8{ 16, 18, 20, 24, 28, 32, 36, 48 };
     var defs = try allocator.alloc(FontDef, sizes.len);
     defer allocator.free(defs);
 
